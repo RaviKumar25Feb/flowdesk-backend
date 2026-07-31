16d652985d87b66d8903b3b523a7c8b9d85ac2b6
ravikumar25feb <RaviKumar25Feb@users.noreply.github.com>
2026-07-31 17:04:59 +0530

    changed cookie

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -58,6 +58,9 @@ exports.login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
+      //production me include these
+      secure: true,
+      sameSite: "none",
     });
 
     return res.status(200).json({
